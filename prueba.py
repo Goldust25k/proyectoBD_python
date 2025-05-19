99b98f9e2535f3a3fafcada8c285816f3949f348
Joaquin L <joaquinlimaco593@gmail.com>
2025-05-19 20:39:37 -0300

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,12 @@
+def suma_columnas(nombre_archivo):
+    suma_col = []
+    with open(nombre_archivo, 'r') as archivo:
+        for linea in archivo:
+            numeros = list(map(int, linea.split()))
+            if len(suma_col) < len(numeros):
+                suma_col.extend([0] * (len(numeros) - len(suma_col)))
+            for i in range(len(numeros)):
+                suma_col[i] += numeros[i]
+    return suma_col
+
+print(suma_columnas('datos1.txt'))
